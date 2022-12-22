66ea936d7c44bf1ebfd55b8d612f2ffed595cbc7
Mathieu Larue <mlarue@mirego.com>
2022-12-22 10:58:50 -0500

    Tweak

--- src/jvmMain/kotlin/Consts.kt
+++ src/jvmMain/kotlin/Consts.kt
@@ -4,5 +4,5 @@ object Consts {
     const val FRAME_DURATION_MS = 16
     val CHARACTER_SIZE = 48.dp
     val MOVEMENT_DISTANCE = 16.dp
-    const val MOVEMENT_DURATION_MS = 148
+    const val MOVEMENT_DURATION_MS = 83
 }
